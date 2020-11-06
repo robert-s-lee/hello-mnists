7c1e28c5bb225e7eff7583168049dc757aa56538
Luis Capelo <luis@gridai.co>
2020-11-06 09:53:34 -0500

    Adds max_epochs Paramete

--- pl_mnist.py
+++ pl_mnist.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('--gpus', type=int, default=None)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--max_epochs', default=100)
     args = parser.parse_args()
 
     dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
@@ -49,5 +50,5 @@ if __name__ == '__main__':
     model = LitModel(lr=args.lr)
 
     # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
-    trainer = pl.Trainer(gpus=args.gpus)
+    trainer = pl.Trainer(gpus=args.gpus, max_epochs=args.max_epochs)
     trainer.fit(model, train_loader)
